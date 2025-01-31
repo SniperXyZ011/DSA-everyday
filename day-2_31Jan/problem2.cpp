3880610e18ae84125ddacca3f02674a211c1e477
Shyam Kumar <shyamprajapatiofficial2011@gmail.com>
2025-01-31 18:47:50 +0530

    Problem2 done

---
+++ day-2_31Jan/problem2.cpp
@@ -0,0 +1,31 @@
+// 242. Valid Anagram
+// Given two strings s and t, return true if t is an 
+// anagram
+//  of s, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+
+// Output: false
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if(s == t){
+            return true;
+        }
+
+        return false;
+    }
