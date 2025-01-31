3908e8b80c3433c1d2c402cbebe9f3abff92444d
Shyam Kumar <shyamprajapatiofficial2011@gmail.com>
2025-01-31 18:05:32 +0530

    Problem1 done

---
+++ day-2_31Jan/problem1.cpp
@@ -0,0 +1,46 @@
+// 1903. Largest Odd Number in String
+// You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+// A substring is a contiguous sequence of characters within a string.
+
+ 
+
+// Example 1:
+
+// Input: num = "52"
+// Output: "5"
+// Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+// Example 2:
+
+// Input: num = "4206"
+// Output: ""
+// Explanation: There are no odd numbers in "4206".
+// Example 3:
+
+// Input: num = "35427"
+// Output: "35427"
+// Explanation: "35427" is already an odd number.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string largestOddNumber(string num) {
+        string ans = "";
+        int n = num.size();
+        int i = 0;
+        int j = -1;
+        while(i < n){
+            char ch = num[i];
+            if((ch - '0')%2 != 0){
+                j = i;
+            }
+            i++;
+        }
+
+        // if(j == 0){
+        //     return ;
+        // }
+        ans = ans + num.substr(0, j + 1);
+        return ans;
+    }
