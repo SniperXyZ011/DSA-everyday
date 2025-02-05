b4e58a81639941ff8cdf9f3d768e9424519fd4e7
Shyam Kumar <shyamprajapatiofficial2011@gmail.com>
2025-02-05 19:57:37 +0530

    problem1 done

---
+++ day-6_feb5/problem1.cpp
@@ -0,0 +1,29 @@
+// Given a Doubly Linked list and a position. The task is to delete a node from a given position (position starts from 1) in a doubly linked list and return the head of the doubly Linked list.
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val) {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val) {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
